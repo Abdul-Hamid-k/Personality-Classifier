a54132be2b565d645e5885a5802ed5ef8c82f1ad
Abdul Hamid Khatri <81006274+Abdul-Hamid-k@users.noreply.github.com>
2026-06-11 18:21:47 +0000

    updated requirement.txt file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 # from pathlib import Path
 # import sys
 # sys.path.append('../')
